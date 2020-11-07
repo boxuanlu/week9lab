1e153b2bce0f80ccf5489b0ae49c66e96160db1e
843827 <843827@SAIT224924IT.ACDM.DS.SAIT.CA>
2020-11-06 19:54:27 -0800

    finish the lab

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -1,15 +1,16 @@
 package servlets;
 
 import services.UserService;
-import models.User;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Users;
 
 public class UserServlet extends HttpServlet {
 
@@ -22,16 +23,16 @@ public class UserServlet extends HttpServlet {
         if (action != null && action.equals("view")) {
             String selectedUsername = request.getParameter("selectedUsername");
             try {
-                User user = us.get(selectedUsername);
+                Users user = us.get(selectedUsername);
                 request.setAttribute("selectedUser", user);
             } catch (Exception ex) {
                 Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         
-        ArrayList<User> users = null;        
+        List<Users> users = null;        
         try {
-            users = (ArrayList<User>) us.getAll();
+            users = (List<Users>) us.getAll();
         } catch (Exception ex) {
             Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -65,9 +66,9 @@ public class UserServlet extends HttpServlet {
             request.setAttribute("errorMessage", "Whoops.  Could not perform that action.");
         }
         
-        ArrayList<User> users = null;
+        List<Users> users = null;
         try {
-            users = (ArrayList<User>) us.getAll();
+            users = (List<Users>) us.getAll();
         } catch (Exception ex) {
             Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
